edb6f3596c01ff3aae033db8235a49b18a6387f7
Aygeeen <wreckingkraken@gmail.com>
2023-08-27 18:39:00 +0600

    fetch

--- js/lesson.js
+++ js/lesson.js
@@ -62,6 +62,7 @@ tabsParent.onclick = (event) => {
 const som = document.querySelector('#som');
 const usd = document.querySelector('#usd');
 const eur = document.querySelector('#eur');
+
 const converter = (element, target1, target2, isTrue) => {
     element.oninput = () => {
         const request = new XMLHttpRequest();
@@ -71,33 +72,79 @@ const converter = (element, target1, target2, isTrue) => {
 
         request.onload = () => {
             const response = JSON.parse(request.response);
+            const rateUSD = response.usd;
+            const rateEuro = response.euro;
+
             if (isTrue) {
-                target1.value = (element.value * response.usd).toFixed(2);
-                target2.value = (element.value * response.euro).toFixed(2);
+                target1.value = (element.value * rateUSD).toFixed(2);
+                target2.value = (element.value * rateEuro).toFixed(2);
             } else {
-                target1.value = (element.value / response.usd).toFixed(2);
-                target2.value = (element.value / response.euro).toFixed(2);
+                target1.value = (element.value / rateUSD).toFixed(2);
+                target2.value = (element.value / rateEuro).toFixed(2);
             }
 
-
             if (element === usd) {
-                target2.value = (element.value * (response.usd / response.euro)).toFixed(2);
+                target2.value = (element.value * (rateUSD / rateEuro)).toFixed(2);
             } else if (element === eur) {
-                target1.value = (element.value * (response.usd / response.euro)).toFixed(2);
+                target1.value = (element.value * (rateUSD / rateEuro)).toFixed(2);
             }
 
             if (element.value === '') {
                 target1.value = '';
                 target2.value = '';
             }
-        }
-    }
-}
+        };
+    };
+};
 
 converter(som, usd, eur, false);
 converter(usd, som, eur, true);
 converter(eur, som, usd, true);
 
-//git
+//----------------------CARD SWITCHER--------------
+const card = document.querySelector(".card");
+const btnPrev = document.querySelector("#btn-prev");
+const btnNext = document.querySelector("#btn-next");
+let count = 1;
+
+const updateCard = (count) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        .then(response => response.json())
+        .then(data => {
+            card.innerHTML = `
+                <p>${data.title}</p>
+                <p style="color: ${data.completed ? 'green' : 'red'}">${data.completed}</p>
+                <span>${data.id}</span>
+            `;
+        });
+};
+const changeCard = (increment) => {
+    count += increment;
+    if (count < 1) {
+        count = 200;
+    } else if (count > 200) {
+        count = 1;
+    }
+    updateCard(count);
+};
+
+btnPrev.onclick = () => {
+    changeCard(-1);
+};
+
+btnNext.onclick = () => {
+    changeCard(1);
+};
+updateCard(count);
 
 
+
+//вторая часть дз
+const qwerty = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+};
+qwerty();
